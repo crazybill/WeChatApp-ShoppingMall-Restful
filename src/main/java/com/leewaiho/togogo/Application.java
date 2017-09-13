72f25124c268a876947670d5bfd346e6f20c9cb9
LeeWaiHo <791783391@qq.com>
2017-09-13 21:29:04 +0800

    引入Redis模块

--- src/main/java/com/leewaiho/togogo/Application.java
+++ src/main/java/com/leewaiho/togogo/Application.java
@@ -2,6 +2,9 @@ package com.leewaiho.togogo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class Application {
@@ -9,4 +12,13 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+	
+	@Bean
+	public RestTemplate restTemplate() {
+		SimpleClientHttpRequestFactory simpleClientHttpRequestFactory = new SimpleClientHttpRequestFactory();
+		simpleClientHttpRequestFactory.setConnectTimeout(60000);
+		simpleClientHttpRequestFactory.setReadTimeout(60000);
+		return new RestTemplate(simpleClientHttpRequestFactory);
+	}
+	
 }
