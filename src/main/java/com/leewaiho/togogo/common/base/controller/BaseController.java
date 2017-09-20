e7270ad4ac4d9d468b52e4a2a2b4edfa988d52ec
LeeWaiHo <791783391@qq.com>
2017-09-20 13:11:08 +0800

    完善了BaseController的机制

--- src/main/java/com/leewaiho/togogo/common/base/controller/BaseController.java
+++ src/main/java/com/leewaiho/togogo/common/base/controller/BaseController.java
@@ -1,6 +1,8 @@
 package com.leewaiho.togogo.common.base.controller;
 
+import com.leewaiho.togogo.common.base.service.BaseService;
 import com.leewaiho.togogo.common.pojo.Result;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,18 +18,30 @@ import java.io.Serializable;
  */
 public abstract class BaseController<T, D extends Serializable> {
     
+    @Autowired
+    private BaseService<T, D> service;
+    
     public static final String BASE_PATH = "/api";
     
     @RequestMapping(value = "/{id:\\d{18}}", method = RequestMethod.GET)
-    public abstract Result findById(@PathVariable D id);
+    public  Result findById(@PathVariable D id) {
+        return Result.success(service.findById(id));
+    }
     
     @RequestMapping(method = RequestMethod.GET)
-    public abstract Result listPage(Pageable pageable);
+    public Result listPage(Pageable pageable) {
+        return Result.success(service.findAll(pageable));
+    }
     
     @RequestMapping(method = RequestMethod.POST)
-    public abstract Result<T> save(T t);
+    public Result<T> save(T t) {
+        return Result.success(service.save(t));
+    }
     
-    @RequestMapping(value = "/{id:\\d{18}}", method = RequestMethod.DELETE)
-    public abstract Result delete(@PathVariable D id);
+    @RequestMapping(value = "/{id:\\d{18}}",method = RequestMethod.DELETE)
+    public Result delete(@PathVariable D id) {
+        service.delete(id);
+        return Result.success(null);
+    }
     
 }
--- src/main/java/com/leewaiho/togogo/module/sys/controller/UserController.java
+++ src/main/java/com/leewaiho/togogo/module/sys/controller/UserController.java
@@ -1,12 +1,7 @@
 package com.leewaiho.togogo.module.sys.controller;
 
 import com.leewaiho.togogo.common.base.controller.BaseController;
-import com.leewaiho.togogo.common.pojo.Result;
 import com.leewaiho.togogo.module.sys.model.TSUser;
-import com.leewaiho.togogo.module.sys.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,24 +15,5 @@ import static com.leewaiho.togogo.common.base.controller.BaseController.BASE_PAT
 @RestController
 @RequestMapping(BASE_PATH + "/users")
 public class UserController extends BaseController<TSUser, String> {
-    
-    @Autowired
-    private UserService userService;
-    
-    public Result findById(@PathVariable String id) {
-        return Result.success(userService.findById(id));
-    }
-    
-    public Result listPage(Pageable pageable) {
-        return Result.success(userService.findAll(pageable));
-    }
-    
-    public Result<TSUser> save(TSUser tsUser) {
-        return Result.success(userService.save(tsUser));
-    }
-    
-    public Result delete(@PathVariable  String id) {
-        userService.delete(id);
-        return Result.success(null);
-    }
+
 }
