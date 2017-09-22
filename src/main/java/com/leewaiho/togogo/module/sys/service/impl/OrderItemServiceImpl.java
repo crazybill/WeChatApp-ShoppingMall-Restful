f62e46c60b0153071267323251edb984da13e9ab
LeeWaiHo <791783391@qq.com>
2017-09-22 15:02:30 +0800

    初始化OrderItemService

---
+++ src/main/java/com/leewaiho/togogo/module/sys/service/impl/OrderItemServiceImpl.java
@@ -0,0 +1,16 @@
+package com.leewaiho.togogo.module.sys.service.impl;
+
+import com.leewaiho.togogo.common.base.service.BaseServiceImpl;
+import com.leewaiho.togogo.module.sys.model.order.TBOrderItem;
+import com.leewaiho.togogo.module.sys.service.OrderItemService;
+import org.springframework.stereotype.Service;
+
+/**
+ * Author leewaiho
+ * Email 791783391@qq.com
+ * Date 2017/9/22
+ * Project togogo-shixun
+ */
+@Service
+public class OrderItemServiceImpl extends BaseServiceImpl<TBOrderItem> implements OrderItemService {
+}
