7aa0928292bccc90849ac4702af3511e5c128259
LeeWaiHo <791783391@qq.com>
2017-09-29 16:40:47 +0800

    修改Result类

--- src/main/java/com/leewaiho/togogo/common/pojo/Result.java
+++ src/main/java/com/leewaiho/togogo/common/pojo/Result.java
@@ -14,17 +14,25 @@ import java.util.Map;
  * @Date 2017/9/13
  */
 public class Result<T> implements Serializable {
-    private String message = "操作成功";
-    private boolean success = true;
+    
+    private String message = Code.SUCCESS.getMessage();
+    private boolean success = Code.SUCCESS.isSuccess();
+    private int code = Code.SUCCESS.getCode();
     private T data;
     
     public Result() {
     }
     
-    public Result(String message, boolean success, T data) {
-        this.message = message;
-        this.success = success;
+    public Result(T data, Code code) {
+        this.success = code.isSuccess();
+        this.code = code.getCode();
+        this.message = code.getMessage();
         this.data = data;
+    }
+    
+    public Result(T data, Code code, String message) {
+        this(data, code);
+        this.message = message;
     }
     
     public String getMessage() {
@@ -41,6 +49,14 @@ public class Result<T> implements Serializable {
     
     public void setSuccess(boolean success) {
         this.success = success;
+    }
+    
+    public int getCode() {
+        return code;
+    }
+    
+    public void setCode(int code) {
+        this.code = code;
     }
     
     public T getData() {
@@ -78,14 +94,57 @@ public class Result<T> implements Serializable {
         return success(data, null);
     }
     
-    
     @Override
     public String toString() {
         return "Result{" +
                        "message='" + message + '\'' +
                        ", success=" + success +
+                       ", code=" + code +
                        ", data=" + data +
                        '}';
+    }
+    
+    public enum Code {
+        FAILED(10000, "操作失败", false), UNLOGIN(11000, "用户未登录", false),
+        UNREGISTER(11010, "用户未注册", false), SUCCESS(20000, "操作成功"),
+        LOGIN(21000, "登录成功"), REGISTERD(21010, "注册成功");
+        
+        private int code;
+        private String message;
+        private boolean success;
+        
+        Code(int code, String message) {
+            this.code = code;
+            this.message = message;
+            this.success = true;
+        }
+        
+        Code(int code, String message, boolean success) {
+            this.code = code;
+            this.message = message;
+            this.success = success;
+        }
+        
+        public int getCode() {
+            return code;
+        }
+        
+        public String getMessage() {
+            return message;
+        }
+        
+        public boolean isSuccess() {
+            return success;
+        }
+        
+        @Override
+        public String toString() {
+            return "Code{" +
+                           "code=" + code +
+                           ", message='" + message + '\'' +
+                           ", success=" + success +
+                           "} " + super.toString();
+        }
     }
     
 }
