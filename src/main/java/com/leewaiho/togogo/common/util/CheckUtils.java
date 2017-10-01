5fd2e030d7894afdbb9a88a1405f1903c08c1cf6
LeeWaiHo <791783391@qq.com>
2017-10-01 18:35:42 +0800

    添加检验工具

---
+++ src/main/java/com/leewaiho/togogo/common/util/CheckUtils.java
@@ -0,0 +1,36 @@
+package com.leewaiho.togogo.common.util;
+
+import com.leewaiho.togogo.common.exception.ServiceException;
+
+import static com.leewaiho.togogo.common.Const.ServiceCode;
+
+/**
+ * Author leewaiho
+ * Email 791783391@qq.com
+ * Date 2017/10/1
+ * Project togogo-shixun
+ */
+public class CheckUtils {
+    
+    public static boolean check(boolean condition, ServiceCode serviceCode, String message) {
+        if (!condition)
+            if (StringUtils.isEmpty(message))
+                throw new ServiceException(serviceCode, message);
+            else
+                throw new ServiceException(serviceCode);
+        return true;
+    }
+    
+    public static boolean check(boolean condition, ServiceCode serviceCode) {
+        return check(condition, serviceCode, null);
+    }
+    
+    public static boolean check(boolean condition, String message) {
+        return check(condition, ServiceCode.BADREQUEST, message);
+    }
+    
+    public static boolean check(boolean condition) {
+        return check(condition, ServiceCode.BADREQUEST, null);
+    }
+    
+}
